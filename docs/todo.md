981502a35b3dd52e1c354fa47808224d5dee7cc2
nemanjam <nemanja.mitic.elfak@hotmail.com>
2023-01-24 13:35:12 +0100

    add styles folder

--- docs/todo.md
+++ docs/todo.md
@@ -3,3 +3,8 @@
 - next.js, flask
 - maybe tRPC https://trpc.io/docs/nextjs
 - monorepo, turborepo, framer motion
+- commit messages, https://www.conventionalcommits.org/en/v1.0.0/
+- components from vercel repo are undefined, https://github.com/vercel/examples/tree/main/packages/ui/src
+- lint, prettier, extensions, scss
+
+
--- services/client/apps/app/pages/_app.tsx
+++ services/client/apps/app/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import type { LayoutProps } from '@vercel/examples-ui/layout'
 import { getLayout } from '@vercel/examples-ui'
-import '@vercel/examples-ui/globals.css'
+import '@styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const Layout = getLayout<LayoutProps>(Component)
